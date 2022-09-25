537c7a75c93f474d1af75b3b8378178ef7429d98
J3remiST <sirjeremaw@gmail.com>
2022-09-25 09:00:34 +0700

    cicil showProfile

---
+++ page/showProfile.php
@@ -0,0 +1,8 @@
+<?php
+    include '../component/sidebar.php'
+?>
+
+<div class="container p-3 m-4 h-100" style="background-color: #FFFFFF; border-top: 5px solid #D40013; ">
+    <h4>SHOW DATA PROFILE</h4>
+    <hr>
+    <div class="container p-3 m-4" style="background-color: #FFFFFF; border-top: 5px solid #314D63; width: 900px; height: 550px;" > 
